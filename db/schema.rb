b1126d7066b35d540d29eec21ef6a4e04771f643
Katherine Worley <kdesign216@yahoo.com>
2017-12-01 17:03:16 -0500

    db migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121205511) do
+ActiveRecord::Schema.define(version: 20171124175927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,13 @@ ActiveRecord::Schema.define(version: 20171121205511) do
     t.string "email", limit: 50
     t.integer "company"
     t.string "title", limit: 50
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "comments", "articles"
